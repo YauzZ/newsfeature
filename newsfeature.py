5a077c7dd56014ed4fd51cad94286907d3a4e987
YauzZ.vm <yauzz.scu@gmail.com>
2009-10-27 10:23:40 +0800

    添加getarticlewords(),测试正常

--- newsfeature.py
+++ newsfeature.py
@@ -3,7 +3,9 @@ import feedparser
 import re
 
 
-feedlist=['http://today.reuters.com/rss/topNews']
+feedlist=[
+       'http://www.gentoo.org/rdf/en/gentoo-news.rdf'
+        ]
 
 
 def stripHTML(h):
@@ -23,4 +25,31 @@ def stripHTML(h):
 def separatewords(text):
     splitter=re.compile('\\W*')
     return [s.lower() for s in splitter.split(text) if len(s)>3 ]
+
+def getarticlewords():
+    allwords={}
+    articlewords=[]
+    articletitles=[]
+    ec=0
+
+    for feed in feedlist:
+        f=feedparser.parse(feed)
+
+        for e in f.entries:
+            if e.title in articletitles:
+                continue
+            txt=e.title.encode('utf8')+stripHTML(e.description.encode('utf8'))
+            words=separatewords(txt)
+            articlewords.append({})
+            articletitles.append(e.title)
+
+            for word in words:
+                allwords.setdefault(word,0)
+                allwords[word]+=1
+                articlewords[ec].setdefault(word,0)
+                articlewords[ec][word]+=1
+
+            ec+=1
+    return allwords,articlewords,articletitles
+
 
