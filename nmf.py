1090e788adb106298c8ae3fa62ba3f98e112121d
YauzZ.vm <yauzz.scu@gmail.com>
2009-10-27 15:45:00 +0800

    添加factorize() #获得权重矩阵和特征矩阵

--- nmf.py
+++ nmf.py
@@ -10,3 +10,35 @@ def difcost(a,b):
             dif+=pow(a[i,j]-b[i,j],2)
 
     return dif
+
+def factorize(v,pc=10,iter=50):
+    ic=shape(v)[0]
+    fc=shape(v)[1]
+
+    w=matrix([[random.random() for j in range(pc)] for i in range(ic)])
+    w=matrix([[random.random() for j in range(fc)] for i in range(pc)])
+
+    for i in range(iter):
+        wh=w*h
+
+        cost=difcost(v,wh)
+
+        if i%10==0:
+            print cost
+
+    if cost==0:
+        break
+
+    hn=(transpose(w)*v)
+    hd=(transpose(w)*w*h)
+
+    h=matrix(array(h)*array(hn)/array(hd))
+
+    wn=(v*transpose(h))
+    wd=(w*h*transpose(h))
+
+    w=matrix(array(w)*array(wn)/array(wd))
+
+    return w,h
+
+
--- test_main.py
+++ test_main.py
@@ -7,8 +7,8 @@ allw,artw,artt=newsfeature.getarticlewords()
 mat,vec=newsfeature.makematrix(allw,artw)
 
 
+print "生成的矩阵："len(i),"x",len(mat)
 for i in mat:
     print i
-print len(i),"x",len(mat)
 
-print len(vec),vec
+
